89d100e105ad4584a734e3f5a90e2c912cd1b76b
Reactician <birzbatis@gmail.com>
2021-05-15 22:28:20 +0300

    Switcheroo and conjured item
    Made the UpdateQuality() "if" mess into a switch mess. It does make it so it is easier to add logic to the new unique items by adding another case. However it is still quite a mess.

--- GildedRoseForInternShip/GildedRoseForInternShip/Program.cs
+++ GildedRoseForInternShip/GildedRoseForInternShip/Program.cs
@@ -8,7 +8,6 @@ namespace csharp
         public static void Main(string[] args)
         {
             Console.WriteLine("OMGHAI!");
-
             IList<Item> Items = new List<Item>{
                 new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
@@ -33,7 +32,7 @@ namespace csharp
                     SellIn = 5,
                     Quality = 49
                 },
-				// this conjured item does not work properly yet
+				// this conjured item does work properly now probably
 				new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
